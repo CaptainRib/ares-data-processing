576d19deb35157317dc5f69b1105f505ec5fcee3
Captain Rib <hjeffreydu@gmail.com>
2023-06-09 14:30:02 -0700

    Add logic for inference in notebook

--- train/pattern_recognition/segementation_generation.py
+++ train/pattern_recognition/segementation_generation.py
@@ -1,13 +1,14 @@
 import matplotlib.dates as mdates
 import plotly.graph_objects as go
 import os
+import pytz
 
 
 class SegmentImageGenerator:
     def __init__(self, data):
         self.data = data
 
-    def _generate_candlestick_image(self, start, segment_size, output_filename):
+    def _generate_candlestick_image(self, start, segment_size, output_dir):
         """
         Generate and save a candlestick chart image from the given DataFrame.
 
@@ -16,9 +17,12 @@ class SegmentImageGenerator:
         :param segment_size: int, the number of consecutive bars to include in the segment
         :param output_filename: str, the path to save the generated image
         """
+
         segment_data = self.data.iloc[start:start + segment_size].reset_index()
         segment_data['timestamp'] = mdates.date2num(segment_data['timestamp'].dt.to_pydatetime())
-
+        timestamp_start = mdates.num2date(segment_data['timestamp'].iloc[0])
+        timestamp_end = mdates.num2date(segment_data['timestamp'].iloc[-1])
+        
         fig = go.Figure(data=[go.Candlestick(x=segment_data['timestamp'],
                     open=segment_data['open'],
                     high=segment_data['high'],
@@ -26,12 +30,24 @@ class SegmentImageGenerator:
                     close=segment_data['close'], increasing_line_color='red', decreasing_line_color='red')])
         
         # Remove slider, set background color to white and remove grid
-        fig.update_layout(xaxis_rangeslider_visible=False, paper_bgcolor='white', xaxis=dict(showgrid=False), yaxis=dict(showgrid=False))
+        fig.update_layout(xaxis_rangeslider_visible=False, paper_bgcolor='white', xaxis=dict(showgrid=False, visible=False), yaxis=dict(showgrid=False, visible=False))
         fig.update_traces(line=dict(width=0))
 
-        fig.write_image(output_filename)
+        timestamp_start_utc = timestamp_start.replace(tzinfo=pytz.UTC)
+        timestamp_end_utc = timestamp_end.replace(tzinfo=pytz.UTC)
 
-    def generate_segmented_images(self, segment_sizes, ticker, date, step=1):
+        new_york_tz = pytz.timezone('America/New_York')
+
+        timestamp_start_ny = timestamp_start_utc.astimezone(new_york_tz)
+        timestamp_end_ny = timestamp_end_utc.astimezone(new_york_tz)
+
+        timestamp_start_str = timestamp_start_ny.strftime('%Y%m%d%H%M%S')
+        timestamp_end_str = timestamp_end_ny.strftime('%Y%m%d%H%M%S')
+        output_filename = os.path.join(output_dir, '{}_{}.png'.format(timestamp_start_str, timestamp_end_str))
+        fig.write_image(output_filename)
+    
+
+    def generate_segmented_images(self, segment_sizes, ticker, output_dir, step=1):
         """
         Generate and save candlestick chart images for multiple segment sizes and start indices.
 
@@ -40,12 +56,10 @@ class SegmentImageGenerator:
         :param step: int, the step size for iterating through the data (default: 1)
         """
         # Find the correct output directory
-        output_base_directory = '/home/jeffreydhy/data/projects/ares-finance/generated/images/unlabeled'
 
         for segment_size in segment_sizes:
-            size_dir = os.path.join(output_base_directory, f"{ticker}_{segment_size}min_segments")
+            size_dir = os.path.join(output_dir, f"{ticker}_{segment_size}min_segments")
             os.makedirs(size_dir, exist_ok=True)
 
             for start in range(0, len(self.data) - segment_size, step):
-                output_filename = os.path.join(size_dir, f"segment_{date}_{start}.png")
-                self._generate_candlestick_image(start, segment_size, output_filename)
+                self._generate_candlestick_image(start, segment_size, size_dir)
--- train/scripts/generate_seg_chart.py
+++ train/scripts/generate_seg_chart.py
@@ -5,15 +5,17 @@ import pandas as pd
 from utils.time_utils import Utility
 from train.pattern_recognition.segementation_generation import SegmentImageGenerator
 
-def generate_img(dates):
+def generate_img(dates, output_dir):
     for date in dates:
         data = pd.read_csv('~/data/projects/ares-finance/generated/bar/amd/{}_1_min.csv'.format(date), parse_dates=['timestamp'])
         segment_sizes = [30]
         seggen = SegmentImageGenerator(data)
-        seggen.generate_segmented_images(segment_sizes, 'AMD', date)
+        seggen.generate_segmented_images(segment_sizes, 'AMD', output_dir)
         
 
+output_base_directory = '/home/jeffreydhy/data/projects/ares-finance/generated/images/inference'
 SYMBOL = 'AMD'
-DATE_START = '2023-04-01'
-DATE_END = '2023-04-30'
+DATE_START = '2023-01-01'
+DATE_END = '2023-04-28'
 dates = Utility.list_trading_days(DATE_START, DATE_END)
+generate_img(dates, output_base_directory)
